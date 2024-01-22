d4a961ba603bc44e75e118630f3ca790341c7150
emanuel.zulian <emanuel.zulian@mat.tn.it>
2024-01-22 11:28:17 +0100

    fine lezione

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Libro } from './domain/Libro';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '22gennaio';
+
+  elenco : string[] = ['lunedì','martedì','mercoledì','giovedì','venerdì','sabato','domenica']
+
+  contatore : number = 0
+
+  titolo : string = "Titotolo di prova"
+
+  elencoLibri : Libro[] = [
+    {titolo : 'titolo1', tipo : 'tipo1'},
+    {titolo : 'titolo2', tipo : 'tipo2'}
+  ]
+
+  onClick() {
+    //this.elenco.push('giorno ' + this.contatore++)
+    this.elenco.push(this.titolo)
+    this.titolo = "Titolo di prova " + this.contatore++
+  }
+
+  onDelete(index : number) {
+    this.elenco.splice(index,1)
+  }
 }
---
+++ src/app/figlia/figlia.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-figlia',
+  templateUrl: './figlia.component.html',
+  styleUrl: './figlia.component.css'
+})
+export class FigliaComponent {
+
+}
